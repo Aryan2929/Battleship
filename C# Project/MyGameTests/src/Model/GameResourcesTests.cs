a3991c4b3f4adab78f26b9962546168d21b0fb56
Ravindu77 <101692013@student.swin.edu.au>
2019-05-08 19:58:45 +1000

    Unit test set 01

---
+++ C# Project/MyGameTests/src/Model/GameResourcesTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BattleShipTests
+{
+    [TestFixture]
+    class GameResourcesTests
+    {
+        [Test]
+        public void Game_Font_Test()
+        {
+            GameResources.LoadResources();
+            Assert.IsNotNull(GameResources.GameFont("Courier"));
+            GameResources.FreeResources();
+        }
+
+        [Test]
+        public void Game_Image()
+        {
+            //GameResources.LoadResources();
+            Assert.IsNotNull(GameResources.GameImage("Explosion"));
+            //GameResources.FreeResources();
+        }
+
+        [Test]
+        public void Game_Sound()
+        {
+            //GameResources.LoadResources();
+            Assert.IsNotNull(GameResources.GameSound("Error"));
+            //GameResources.FreeResources();
+        }
+
+        [Test]
+        public void Game_Music()
+        {
+            //GameResources.LoadResources();
+            Assert.IsNull(GameResources.GameMusic("Background"));
+            //GameResources.FreeResources();
+        }
+    }
+}
